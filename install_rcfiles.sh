f0a6d44d37a645b63bd6b8ccdee3473ef3bdebcd
Mikhail Itkin <itkin.m@gmail.com>
2014-10-23 15:07:46 +0200

    added Xdefaults to the array of files to be installed

--- install_rcfiles.sh
+++ install_rcfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOTFILESDIR=$(pwd)
-files=(tmux.conf bashrc gitconfig zshrc)
+files=(tmux.conf bashrc gitconfig zshrc Xdefaults)
 for i in ${files[@]}; do
     ln -sfv ${DOTFILESDIR}/$i $HOME/.$i
 done
