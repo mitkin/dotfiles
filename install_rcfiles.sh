e3083f9f89f1a871b8a741ad4eae49a8e3569af1
Mikhail Itkin <itkin.m@gmail.com>
2014-10-23 13:06:14 +0000

    bash-like array parsing

--- install_rcfiles.sh
+++ install_rcfiles.sh
@@ -2,7 +2,7 @@
 
 DOTFILESDIR=$(pwd)
 files=(tmux.conf bashrc gitconfig zshrc)
-for i in ${files}; do
-    ln -sf ${DOTFILESDIR}/$i $HOME/.$i
+for i in ${files[@]}; do
+    ln -sfv ${DOTFILESDIR}/$i $HOME/.$i
 done
 
